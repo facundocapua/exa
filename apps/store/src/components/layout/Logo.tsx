d36a9edce8c8b1a0d08f6c8cfb862a3789fb093d
Facundo Capua <facundocapua@gmail.com>
2023-10-06 18:37:30 -0300

    feat(logo): update logo

--- apps/store/src/components/layout/Logo.tsx
+++ apps/store/src/components/layout/Logo.tsx
@@ -4,10 +4,7 @@ import Link from 'next/link'
 export default function Logo () {
   return (
     <Link href="/">
-      {/* <h1 className="text-4xl tracking-wider font-logo text-black font-extralight">
-        e<span className="font-bold">X</span>a
-      </h1> */}
-      <Image src="/logo.png" width={100} height={74} alt="eXa" />
+      <Image src="/logo.png" width={128} height={75} alt="eXa" />
     </Link>
   )
 }
