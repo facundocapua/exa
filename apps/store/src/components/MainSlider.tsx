48ebdf2cddccea596d9ad2a86e463159981b9336
Facundo Capua <facundocapua@gmail.com>
2023-10-25 18:43:28 -0300

    feat(exa): change banner home

--- apps/store/src/components/MainSlider.tsx
+++ apps/store/src/components/MainSlider.tsx
@@ -3,20 +3,14 @@ import { HeroSlider } from 'ui'
 export default function MainSlider () {
   const slides = [
     {
-      image: 'https://via.placeholder.com/2500x600',
-      imageMobile: 'https://via.placeholder.com/780x546'
+      image: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner1.png',
+      imageMobile: 'https://via.placeholder.com/780x546',
+      link: '/product/planchita-iht-x-wide-digital'
     },
     {
       image: 'https://via.placeholder.com/2500x600',
-      imageMobile: 'https://via.placeholder.com/780x546'
-    },
-    {
-      image: 'https://via.placeholder.com/2500x600',
-      imageMobile: 'https://via.placeholder.com/780x546'
-    },
-    {
-      image: 'https://via.placeholder.com/2500x600',
-      imageMobile: 'https://via.placeholder.com/780x546'
+      imageMobile: 'https://via.placeholder.com/780x546',
+      link: '#'
     }
   ]
 
