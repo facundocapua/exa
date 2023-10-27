c6868c0d29f83866ab9d464c371bd7152067a5b5
Facundo Capua <facundocapua@gmail.com>
2023-10-27 18:32:17 -0300

    feat(slider): change main slider images

--- apps/store/src/components/MainSlider.tsx
+++ apps/store/src/components/MainSlider.tsx
@@ -3,14 +3,14 @@ import { HeroSlider } from 'ui'
 export default function MainSlider () {
   const slides = [
     {
-      image: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner1.png',
-      imageMobile: 'https://via.placeholder.com/780x546',
-      link: '/product/planchita-iht-x-wide-digital'
-    },
-    {
-      image: 'https://via.placeholder.com/2500x600',
+      image: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner3.png',
       imageMobile: 'https://via.placeholder.com/780x546',
       link: '#'
+    },
+    {
+      image: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner2.png',
+      imageMobile: 'https://via.placeholder.com/780x546',
+      link: '/product/planchita-iht-x-wide-digital'
     }
   ]
 
