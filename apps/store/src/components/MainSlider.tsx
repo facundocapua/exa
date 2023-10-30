af87aaf0744ed3420e27d06385962e6c4dbec29b
Facundo Capua <facundocapua@gmail.com>
2023-10-30 19:05:17 -0300

    feat(home): hero slider

--- apps/store/src/components/MainSlider.tsx
+++ apps/store/src/components/MainSlider.tsx
@@ -4,12 +4,12 @@ export default function MainSlider () {
   const slides = [
     {
       image: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner3.png',
-      imageMobile: 'https://via.placeholder.com/780x546',
+      imageMobile: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner3Celu.png',
       link: '#'
     },
     {
       image: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner2.png',
-      imageMobile: 'https://via.placeholder.com/780x546',
+      imageMobile: 'https://mtelzvckwsdkcqbvwewx.supabase.co/storage/v1/object/public/assets/banners/Banner2Celu.png',
       link: '/product/planchita-iht-x-wide-digital'
     }
   ]
--- packages/ui/hero-slider/ForwardButton.tsx
+++ packages/ui/hero-slider/ForwardButton.tsx
@@ -8,7 +8,7 @@ export default function ForwardButton ({ onClick }: Props) {
   return (
     <button
         type="button"
-        className="absolute top-0 right-0 z-10 items-center justify-center hidden h-full px-4 cursor-pointer md:flex group focus:outline-none"
+        className="absolute top-0 right-0 z-10 items-center justify-center h-full px-4 cursor-pointer flex group focus:outline-none"
         onClick={onClick}
       >
       <span className="inline-flex items-center justify-center w-8 h-8 border-2 border-gray-300 rounded-full sm:w-10 sm:h-10 bg-gray-400/10 group-hover:bg-gray-400/20 group-hover:scale-105 group-focus:outline-none">
