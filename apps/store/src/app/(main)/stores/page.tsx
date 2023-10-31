f127654c0fda3228ebc57547165a916bab7aa980
Facundo Capua <facundocapua@gmail.com>
2023-10-31 17:33:08 -0300

    feat(stores): improve design for title

--- apps/store/src/app/(main)/stores/page.tsx
+++ apps/store/src/app/(main)/stores/page.tsx
@@ -1,11 +1,23 @@
 import StoreLocator from '@/components/stores/StoreLocator'
 import { getStores } from 'api/store'
+import { Breadcrumb } from 'ui/server'
 
 export default async function Stores () {
   const stores = await getStores()
+  const brecrumbs = [{
+    name: 'Buscador de Salones',
+    url: '/stores',
+    current: true
+  }]
+
   return (
-    <main className="my-12">
-      <h1 className="text-2xl font-bold text-center">Buscador de Salones</h1>
+    <main className="mx-auto max-w-2xl px-4 lg:max-w-7xl my-4">
+      <Breadcrumb pages={brecrumbs} />
+
+      <div className="pt-12 pb-6 mb-6 border-b border-neutral-300 dark:border-neutral-500">
+        <h1 className="text-4xl font-bold tracking-tight border-neutral-900">Buscador de Salones</h1>
+      </div>
+
       <StoreLocator stores={stores} />
     </main>
   )
