6fdc3a9455b42b0971638296d438db038f64d0f4
Facundo Capua <facundocapua@gmail.com>
2023-11-02 20:01:09 -0300

    feat(ga): improvements to homepage

--- apps/pros/gonzaloalonso/src/components/Portfolio.tsx
+++ apps/pros/gonzaloalonso/src/components/Portfolio.tsx
@@ -60,7 +60,7 @@ export default function Portfolio ({ images }: Props) {
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <div className="relative">
-                    {image !== '' && (<Image src={images[image]} width={1500} height={1000} alt="image" />)}
+                    {image !== '' ? (<Image src={images[Number(image)]} width={1500} height={1000} alt="image" />) : null}
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
