3cfeaba1af8eba70ddc0437f302cbfab7b25b167
Facundo Capua <facundocapua@gmail.com>
2023-11-13 18:58:10 -0300

    chore: ignore ts in textWrap css property

--- apps/pros/gonzaloalonso/src/components/AboutMe.tsx
+++ apps/pros/gonzaloalonso/src/components/AboutMe.tsx
@@ -7,8 +7,11 @@ export default function AboutMe () {
       <header className='w-full md:w-1/2 px-2 md:px-4 z-10'>
         <h1 className='text-5xl font-semibold leading-loose'>Gonzalo <span className='text-primary-300'>Alonso</span></h1>
         <div className='flex flex-col gap-6 px-2 md:px-0'>
+          {/* @ts-ignore */}
           <p className='text-bal' style={{ textWrap: 'balance' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed facilisis libero. Suspendisse in nibh et elit fringilla auctor eget eu erat. Mauris eget maximus urna, ut pharetra odio. Nullam a feugiat odio. Suspendisse nec urna augue. Fusce ut iaculis mi. Quisque ac laoreet metus. Vivamus sodales ex et dui imperdiet placerat. Integer ut augue enim. Sed hendrerit vel nibh sit amet finibus.</p>
+          {/* @ts-ignore */}
           <p style={{ textWrap: 'balance' }}>Phasellus eget molestie ex. Integer sit amet luctus lacus, vel iaculis tellus. Sed elit magna, vehicula at placerat non, fermentum at metus. In at urna efficitur, interdum ante ac, cursus ex. Vestibulum mattis arcu vel libero ornare finibus. Nulla facilisi. Sed mollis nibh tincidunt rhoncus lacinia. Cras vel lacus finibus, lacinia elit ac, gravida velit. Nam eget convallis lacus, ut laoreet magna. Maecenas arcu purus, posuere sed risus nec, fermentum scelerisque augue. Aenean porta mi non nunc consectetur, volutpat sagittis massa lacinia.</p>
+          {/* @ts-ignore */}
           <p style={{ textWrap: 'balance' }}>Pellentesque pharetra velit non est tincidunt, nec convallis velit ornare. Vivamus at turpis tellus. Mauris dignissim, dolor non efficitur interdum, urna metus eleifend purus, a vestibulum dolor quam sit amet lorem. Sed non enim nec orci blandit commodo non non nunc. Vestibulum eget rutrum felis, ut molestie sapien. Fusce molestie mattis ante sit amet varius. Duis vitae pretium risus. In hendrerit eros arcu, in ornare enim laoreet blandit. Curabitur congue velit velit, sit amet eleifend orci placerat sollicitudin. Vivamus eget aliquam sapien, vitae tincidunt lectus. Aliquam erat volutpat.</p>
         </div>
       </header>
