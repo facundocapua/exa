1044ec1b401b8f776e7b736febb5cc23479d973d
Facundo Capua <facundocapua@gmail.com>
2023-11-16 20:28:36 -0300

    feat(ga): refactor image classes in Gallery component

--- apps/pros/gonzaloalonso/src/components/Gallery.tsx
+++ apps/pros/gonzaloalonso/src/components/Gallery.tsx
@@ -9,12 +9,12 @@ type Props = {
 }
 
 const imagesClasses = [
-  'w-full block h-[350px] md:h-full md:min-h-[320px] rounded-xl md:row-span-2 relative',
-  'w-full block h-[350px] md:h-full md:min-h-[320px] rounded-xl md:col-span-2 relative',
-  'w-full block h-[350px] md:h-full md:min-h-[320px] rounded-xl relative',
-  'w-full block h-[350px] md:h-full md:min-h-[320px] rounded-xl md:row-span-2 relative',
-  'w-full block h-[350px] md:h-full md:min-h-[320px] rounded-xl relative',
-  'w-full block h-[350px] md:h-full md:min-h-[320px] rounded-xl relative'
+  'w-full block h-[350px] md:h-full rounded-xl md:row-span-2 relative',
+  'w-full block h-[350px] md:h-full rounded-xl md:col-span-2 relative',
+  'w-full block h-[350px] md:h-full rounded-xl relative',
+  'w-full block h-[350px] md:h-full rounded-xl md:row-span-2 relative',
+  'w-full block h-[350px] md:h-full rounded-xl relative',
+  'w-full block h-[350px] md:h-full rounded-xl relative'
 ]
 
 export default function Gallery ({ images }: Props) {
