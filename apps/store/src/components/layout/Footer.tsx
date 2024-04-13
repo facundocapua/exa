3dfc6c9abe71b3982a4e72572207208e66f044fc
Facundo Capua <facundocapua@gmail.com>
2024-04-13 09:16:59 -0300

    feat: footer info

--- apps/store/src/components/layout/Footer.tsx
+++ apps/store/src/components/layout/Footer.tsx
@@ -5,24 +5,13 @@ import { createElement } from 'react'
 const navigation = {
   solutions: [
     { name: 'Términos y condiciones', href: '/terminos-y-condiciones' },
-    { name: 'Preguntas frecuentes', href: '#' },
-    { name: '¿Cómo comprar?', href: '#' }
+    { name: 'Arrepentimiento', href: '/arrepentimiento' }
+    // { name: '¿Cómo comprar?', href: '#' }
   ],
   support: [
-    { name: 'Contáctanos', href: '#' },
-    { name: 'Sobre nosotros', href: '#' }
-  ],
-  company: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' }
-  ],
-  legal: [
-    { name: 'Claim', href: '#' },
-    { name: 'Privacy', href: '#' },
-    { name: 'Terms', href: '#' }
+    { name: 'tel: 249 420-9997 ', href: 'tel:5492494209997' },
+    { name: 'whatsapp: +549 249 420-9997 ', href: 'https://api.whatsapp.com/send?phone=5492494209997&text=' },
+    { name: 'email: info@exabeauty.com.ar', href: 'mailto:info@exabeauty.com.ar' }
   ],
   social: [
     {
@@ -50,12 +39,14 @@ export default function Footer () {
         Footer
       </h2>
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-          <div className="space-y-8">
-            <Logo responsive={false} center={false} />
-            <p className="text-sm leading-6 text-gray-600">
-              Making the world a better place through constructing elegant hierarchies.
-            </p>
+        <div className="md:grid md:grid-cols-2 md:gap-8">
+          <div className="space-y-4">
+            <div className='flex flex-col gap-y-1 justify-center w-fit items-center'>
+              <Logo responsive={false} center={false} />
+              <p className="text-sm text-gray-600 font-medium">
+                Beauty Solutions
+              </p>
+            </div>
             <div className="flex space-x-6">
               {navigation.social.map((item) => (
                 <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
@@ -65,58 +56,33 @@ export default function Footer () {
               ))}
             </div>
           </div>
-          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.solutions.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="mt-10 md:mt-0">
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.support.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+          <div className="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <ul role="list" className="space-y-4">
+                {navigation.solutions.map((item) => (
+                  <li key={item.name}>
+                    <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.company.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="mt-10 md:mt-0">
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.legal.map((item) => (
-                    <li key={item.name}>
-                      <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                        {item.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+            <div className="mt-10 md:mt-0">
+              <p className="text-sm leading-6 text-gray-600 font-medium">Contacto</p>
+              <ul role="list" className="space-y-2 mt-4">
+                {navigation.support.map((item) => (
+                  <li key={item.name}>
+                    <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
-        <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
+        <div className="mt-8 border-t border-gray-900/10 pt-8">
           <p className="text-xs leading-5 text-gray-500">&copy; 2024 eXa Beauy Solutions, todos los derechos reservados</p>
         </div>
       </div>
