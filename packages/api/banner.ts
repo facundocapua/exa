45b1d014da500a70e5526a7527985bd8cf322bf8
Facundo Capua <facundocapua@gmail.com>
2024-04-18 18:52:27 -0300

    fix: remove sleep

--- packages/api/banner.ts
+++ packages/api/banner.ts
@@ -2,7 +2,7 @@ import { sleep } from './utils/general'
 import { getMedusaUrl } from './utils/medusa'
 
 export const getFeaturedBanners = async () => {
-  await sleep(3000)
+  // await sleep(3000)
   const banners = fetch(`${getMedusaUrl()}/store/banners`)
     .then((res) => res.json())
     .then(data => {
