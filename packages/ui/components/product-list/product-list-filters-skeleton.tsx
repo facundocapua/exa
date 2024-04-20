5ba68e49f9af38366b2a3c44817ef04066c60b8a
Facundo Capua <facundocapua@gmail.com>
2024-04-20 13:04:23 -0300

    feat(category): add product list skeleton

---
+++ packages/ui/components/product-list/product-list-filters-skeleton.tsx
@@ -0,0 +1,14 @@
+export const ProductListFiltersSkeleton = () => {
+  return (
+    <div className="flex flex-col space-y-4">
+      <div className="w-1/4 h-4 bg-gray-200 rounded"></div>
+      <div className="w-1/2 h-4 bg-gray-200 rounded"></div>
+      <div className="w-1/3 h-4 bg-gray-200 rounded"></div>
+      <div className="w-1/4 h-4 bg-gray-200 rounded"></div>
+      <div className="w-1/2 h-4 bg-gray-200 rounded"></div>
+      <div className="w-1/3 h-4 bg-gray-200 rounded"></div>
+    </div>
+  )
+}
+
+export default ProductListFiltersSkeleton
