27f0a84a42b3859e37fad276599afb32a3bfab3a
Facundo Capua <facundocapua@gmail.com>
2024-05-21 10:21:54 +0200

    feat: update top banner free shipping price

--- apps/store/src/components/BannerTop.tsx
+++ apps/store/src/components/BannerTop.tsx
@@ -1,7 +1,7 @@
 export default function BannerTop () {
   return (
     <div className="w-full text-center text-white text-xs font-semibold bg-black py-3">
-      Envío gratis superando $29.999
+      Envío gratis superando $39.999
     </div>
   )
 }
