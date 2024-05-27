08ee5cb81456eb222f03fcead6d1c219bdce861c
Facundo Capua <facundocapua@gmail.com>
2024-05-27 10:03:46 +0200

    fix(checkout): integration with mercadopago

--- apps/store/next.config.js
+++ apps/store/next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.mercadopago.com *.mlstatic.com;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: *.mercadopago.com *.mlstatic.com *.mercadolibre.com *.mercadolivre.com;
-    font-src 'self' *.mlstatic.com;
-    connect-src 'self' *.mercadopago.com *.mercadolibre.com;
-    frame-src 'self' *.mercadopago.com *.mercadolibre.com;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-`
-
 const nextConfig = {
   experimental: {
     serverActions: true
@@ -34,19 +19,6 @@ const nextConfig = {
         hostname: 'cdn.exabeauty.com.ar',
         port: '',
         pathname: '/**'
-      }
-    ]
-  },
-  async headers () {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, '')
-          }
-        ]
       }
     ]
   }
