83c83e3ea64492eddfb596ded56b510ad2f85a5e
Facundo Capua <facundocapua@gmail.com>
2024-06-24 13:53:51 -0300

    feat(fergutirrez): update site

---
+++ apps/pros/fergutierrez/src/components/services/services.tsx
@@ -0,0 +1,12 @@
+import { SERVICES } from '@/data/services'
+import ServiceCard from './service-card'
+
+export default function Services () {
+  return (
+    <section className='grid grid-cols-1 md:grid-cols-4 gap-8 max-w-7xl mx-auto my-12'>
+      {SERVICES.map((service) => (
+        <ServiceCard key={service.title} {...service} />
+      ))}
+    </section>
+  )
+}
