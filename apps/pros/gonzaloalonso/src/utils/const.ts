d3a2f43e3c38d1810c9542671340b80bd8e5efcb
Facundo Capua <facundocapua@gmail.com>
2024-06-24 10:44:26 -0300

    feat(gonzaloalonso): order shipped email and change site description

--- apps/pros/gonzaloalonso/src/utils/const.ts
+++ apps/pros/gonzaloalonso/src/utils/const.ts
@@ -1,4 +1,4 @@
 export const STORE_NAME = 'Gonzalo Alonso'
-export const STORE_DESCRIPTION = 'Gonzalo Alonso Studio - Lo que vos mereces.'
+export const STORE_DESCRIPTION = 'Gonzalo Alonso Studio'
 export const STORE_URL = 'https://gonzaloalonso.exapro.com.ar'
 export const STORE_OG_IMAGE = 'https://cdn.exabeauty.com.ar/ga-og.jpg'
