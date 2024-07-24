79c70ef8fe6c0eaf45fab7652c5c02d334efbb02
Facundo Capua <facundocapua@gmail.com>
2024-07-24 17:47:01 -0300

    feat(sol): adjust meta description

--- apps/pros/solpeluquera/src/utils/const.ts
+++ apps/pros/solpeluquera/src/utils/const.ts
@@ -1,4 +1,4 @@
 export const STORE_NAME = 'Sol La Peluquera'
-export const STORE_DESCRIPTION = 'Sol La Peluquera - Una pelu diferente'
+export const STORE_DESCRIPTION = 'Con más de 20 años de experiencia, se destaca por su expertise en color y especialización en rulos, ofreciendo un estilo vanguardista y libre que transforma cada look en una obra de arte única.'
 export const STORE_URL = 'https://solpeluquera.exapro.com.ar'
 export const STORE_OG_IMAGE = 'https://cdn.exabeauty.com.ar/sol-og.jpg'
