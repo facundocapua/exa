10efb4cd4aa8d224a075a7c33960fc6f8164f77c
Facundo Capua <facundocapua@gmail.com>
2024-10-26 13:01:21 -0300

    feat(carolinapereyra): adjust meta description

--- apps/pros/carolinapereyra/src/utils/const.ts
+++ apps/pros/carolinapereyra/src/utils/const.ts
@@ -1,4 +1,4 @@
 export const STORE_NAME = 'Carolina Pereyra Estilista'
-export const STORE_DESCRIPTION = 'Con más de 20 años de experiencia, se destaca por su expertise en color y especialización en rulos, ofreciendo un estilo vanguardista y libre que transforma cada look en una obra de arte única.'
+export const STORE_DESCRIPTION = 'Estilista apasionada y madre dedicada, con un don especial para realzar la belleza de cada persona que pasa por su salón.'
 export const STORE_URL = 'https://carolinapereyra.exapro.com.ar'
 export const STORE_OG_IMAGE = 'https://cdn.exabeauty.com.ar/carolinapereyra-og.jpg'
