c6c6b96cc6d287ee41610be96740c27a64d7ff91
Facundo Capua <facundocapua@gmail.com>
2024-11-04 08:55:07 -0300

    feat: text color for dark mode

--- packages/ui/components/product-card/ProductPrice.tsx
+++ packages/ui/components/product-card/ProductPrice.tsx
@@ -23,7 +23,7 @@ export default function ProductPrice ({ price, salePrice, containerClassName, pr
         <Price amount={salePrice} className={clsx(twMerge('text-lg text-neutral-600 dark:text-neutral-300', priceClassName))} />
         { salePrice < price && <Price amount={price} className={clsx(twMerge('line-through text-neutral-500 dark:text-neutral-300 text-sm', salePriceClassName))} /> }
       </div>
-      <div className='flex items-center gap-x-2 text-neutral-600 text-sm'>
+      <div className='flex items-center gap-x-2 text-neutral-600 dark:text-neutral-400 text-sm'>
         <CreditCardIcon className='w-4 h-4 text-neutral-600 dark:text-neutral-300 hidden md:block' />
         3 cuotas de <Price amount={salePrice / 3} className='text-primary-700 dark:text-neutral-300 text-base font-semibold' />
       </div>
